9c4b26455f9d1904f259f2ee1c9ac0a062742d4a
Álvaro <alvaritovg93@hotmail.com>
2019-05-13 19:54:04 +0200

    Añadida variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:JvXlz2pLeaum5lEf@cluster0-ht1un.mongodb.net/cave';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
